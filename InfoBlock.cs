c13349e05e7f982e7b561eea2968a6ccecf58e3c
Bmantaz <Bjarkepa@gmail.com>
2025-04-28 11:11:51 +0200

    Info Block indsættes
    scoreboard gøres klar

---
+++ InfoBlock.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+namespace TicTacToe_2804_H1
+{
+    internal class InfoBlock : INotifyPropertyChanged
+    {
+        private int _xWins;
+        private int _oWins;
+
+        public int XWins
+        {
+            get => _xWins;
+            private set
+            {
+                if (_xWins != value)
+                {
+                    _xWins = value;
+                    OnPropertyChanged(nameof(XWins));
+                    OnPropertyChanged(nameof(ScoreText));
+                }
+            }
+        }
+
+        public int OWins
+        {
+            get => _oWins;
+            private set
+            {
+                if (_oWins != value)
+                {
+                    _oWins = value;
+                    OnPropertyChanged(nameof(OWins));
+                    OnPropertyChanged(nameof(ScoreText));
+                }
+            }
+        }
+
+        // Den tekst vi binder til TextBlock.Text
+        public string ScoreText => $"Player X: {XWins} | Player O: {OWins}";
+
+        // Kaldes fra MainWindow, når X eller O vinder
+        public void RegisterWin(bool isXPlayer)
+        {
+            if (isXPlayer) XWins++;
+            else OWins++;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string prop) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,103 @@ using System.Windows.Shapes;
 
 namespace TicTacToe_2804_H1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
+
     {
+        private readonly InfoBlock _info = new InfoBlock();
+        private bool isXTurn = true;
+        private Button[,] board;
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = _info;
+            board = new Button[3, 3]
+            {
+                { Btn00, Btn01, Btn02 },
+                { Btn10, Btn11, Btn12 },
+                { Btn20, Btn21, Btn22 }
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            if (isXTurn)
+                btn.Content = "X";
+            else
+                btn.Content = "O";
+
+            btn.IsEnabled = false;
+
+            if (CheckWin())
+            {
+                MessageBox.Show($"Spiller {(isXTurn ? 'X' : 'O')} har vundet!", "Game Over");
+                ResetBoard();
+                return;
+            }
+
+            if (IsDraw())
+            {
+                MessageBox.Show("Uafgjort!", "Game Over");
+                ResetBoard();
+                return;
+            }
+
+            isXTurn = !isXTurn;
+
+            if (CheckWin())
+            {
+                // Før du nulstiller brættet, registrer vinderen
+                _info.RegisterWin(isXTurn);
+                MessageBox.Show($"Spiller {(isXTurn ? 'X' : 'O')} har vundet!", "Game Over");
+                ResetBoard();
+                return;
+            }
+        }
+
+        private bool CheckWin()
+        {
+            // Tjek rækker og kolonner
+            for (int i = 0; i < 3; i++)
+            {
+                if (Match(board[i, 0], board[i, 1], board[i, 2]) ||
+                    Match(board[0, i], board[1, i], board[2, i]))
+                    return true;
+            }
+            // Tjek diagonaler
+            if (Match(board[0, 0], board[1, 1], board[2, 2]) ||
+                Match(board[0, 2], board[1, 1], board[2, 0]))
+                return true;
+
+            return false;
+        }
+
+        private bool Match(Button a, Button b, Button c)
+        {
+            return a.Content != null &&
+                   a.Content.Equals(b.Content) &&
+                   b.Content.Equals(c.Content);
+        }
+
+        private bool IsDraw()
+        {
+            foreach (var btn in board)
+            {
+                if (btn.IsEnabled)
+                    return false;
+            }
+            return true;
+        }
+
+        private void ResetBoard()
+        {
+            foreach (var btn in board)
+            {
+                btn.Content = null;
+                btn.IsEnabled = true;
+            }
+            isXTurn = true;
         }
     }
 }
